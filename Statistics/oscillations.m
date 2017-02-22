617f0bdfe6b4fc99831648a5521f20056eba2dce
Alexandr Sokolov <sokolovalexv@gmail.com>
2017-02-22 01:42:52 +0100

    update oscillation

--- Statistics/oscillations.m
+++ Statistics/oscillations.m
@@ -1,23 +1,65 @@
 % Oscillations
 
-T = PERI365;
+% FD_ALP_1.EVT
+PERI365.Properties.VariableNames = {'Name','code','flag','event','Period','North','East','Up','S','remark'};
 
-names_t = cell2mat(T.ACCE);
-names_t = names_t(:,2:5);
-names_all
 
-r = [1:length(names_all)]';
+% save('PERI365','PERI365')
+% save('PERP365','PERP365')
 
-ii = r(ismember(names_all, names_t))
+
+% load('PERI365')
+% load('PERP365')
+
+%%
+
+names_t = cell2mat(PERI365.Name);
+
+r = [1:length(names_t)]';
+ii = r(ismember(names_t, names))
+%%
+close all
+figure(5)
+subplot(1,3,1)
+hold on
+Earth_coast(2)
+xlim([-4.5 17])
+ylim([41.8 52])
+scatter(wrapTo180(long), lat, PERI365.East(ii)*10000+1, PERI365.East(ii)*1000 ,'filled' )
+title('East')
+c = colorbar;
+ylabel(c,'mm')
+caxis([0 5])
+
+subplot(1,3,2)
+hold on
+Earth_coast(2)
+xlim([-4.5 17])
+ylim([41.8 52])
+scatter(wrapTo180(long), lat, PERI365.North(ii)*10000+1, PERI365.North(ii)*1000 ,'filled' )
+title('North')
+% text(wrapTo180(long), lat, names)
+c = colorbar;
+ylabel(c,'mm')
+caxis([0 5])
+
+subplot(1,3,3)
+hold on
+Earth_coast(2)
+xlim([-4.5 17])
+ylim([41.8 52])
+scatter(wrapTo180(long), lat, PERI365.Up(ii)*10000+1, PERI365.Up(ii)*1000 ,'filled' )
+title('Up')
+c = colorbar;
+ylabel(c,'mm')
+caxis([0 5])
+
+
+
 
 %%
 clc
 close all
-
-
-%%
-
-ii = ii(1:274);
 
 % Defaults for this blog post
 width = 10;     % Width in inches
@@ -67,27 +109,35 @@ set(0, 'defaultFigurePaperPosition', defsize);
 close all
 fig3 = figure(3);
 colormap('hsv')
-subplot(1,9,[1 2 3]); hold on
-scatter(wrapTo180(long_all(ii)), lat_all(ii), PERI365.VarName8*10000+1, PERI365.VarName8*1000 ,'filled' )
+subplot(1,10,[1 2 3]); hold on
+scatter(wrapTo180(long_all(ii)), lat_all(ii), PERI365.East*10000+1, PERI365.East*1000 ,'filled' )
 title('East')
-c = colorbar;
-ylabel(c,'mm')
+% c = colorbar;
+% ylabel(c,'mm')
+caxis([0 5])
 Earth_coast(2)
 xlim([-4.5 17])
 ylim([41.8 52])
-subplot(1,9,[4 5 6]); hold on
-scatter(wrapTo180(long_all(ii)), lat_all(ii), PERI365.VarName9*10000+1, PERI365.VarName9*1000 ,'filled' )
+subplot(1,10,[4 5 6]); hold on
+scatter(wrapTo180(long_all(ii)), lat_all(ii), PERI365.North*10000+1, PERI365.North*1000 ,'filled' )
 title('North')
-c = colorbar;
-ylabel(c,'mm')
+% c = colorbar;
+% ylabel(c,'mm')
+caxis([0 5])
 Earth_coast(2)
 xlim([-4.5 17])
 ylim([41.8 52])
-subplot(1,9,[ 7 8 9 ]); hold on
-scatter(wrapTo180(long_all(ii)), lat_all(ii), PERI365.VarName10*10000+1, PERI365.VarName10*1000 ,'filled' )
+subplot(1,10,[ 7 8 9 10 ]); hold on
+%%
+close all
+figure(4); hold on
+scatter(wrapTo180(long_all(ii)), lat_all(ii), PERI365.Up*10000+1, PERI365.Up*1000 ,'filled' )
+text(wrapTo180(long_all(ii)), lat_all(ii)+0.2, names_all(ii))
+text(wrapTo180(long_all(ii)), lat_all(ii), num2str(PERI365.Up*1000, '%3.1f'))
 title('Up')
-c = colorbar;
-ylabel(c,'mm')
+s = colorbar;
+ylabel(s,'mm')
+caxis([0 5])
 Earth_coast(2)
 xlim([-4.5 17])
 ylim([41.8 52])
@@ -97,17 +147,45 @@ close all
 fig3 = figure(3);
 colormap('hsv')
 subplot(2,9,[10 11 ]); hold on
-scatter(wrapTo180(long_all(ii)), lat_all(ii), 5*ones(274,1), PERP365.VarName8 ,'filled' )
+scatter(wrapTo180(long_all(ii)), lat_all(ii), 5*ones(274,1), PERP365.East ,'filled' )
 title('East')
 Earth_coast(2)
 xlim([-4.5 17])
 ylim([41 53])
 subplot(2,9, [12 13]); hold on
-scatter(wrapTo180(long_all(ii)), lat_all(ii), 5*ones(274,1), PERP365.VarName9 ,'filled' )
+scatter(wrapTo180(long_all(ii)), lat_all(ii), 5*ones(274,1), PERP365.North ,'filled' )
 title('North')
 xlim([-4.5 17])
 subplot(2,9 ,[14 15 16 ]); hold on
-scatter(wrapTo180(long_all(ii)), lat_all(ii), 5*ones(274,1), PERP365.VarName10 ,'filled' )
+scatter(wrapTo180(long_all(ii)), lat_all(ii), 5*ones(274,1), PERP365.Up ,'filled' )
+title('Up')
+xlim([-4.5 17])
+colorbar
+
+%%
+
+close all
+fig3 = figure(3);
+colormap('hsv')
+subplot(1,4,1); hold on
+s = 40;
+scatter(wrapTo180(long_all(ii)), lat_all(ii), s*ones(274,1), PERI365.East*1000 ,'filled' )
+title('East')
+Earth_coast(2)
+xlim([-4.5 17])
+ylim([41 53])
+subplot(1,4,2 ); hold on
+Earth_coast(2)
+xlim([-4.5 17])
+ylim([41 53])
+scatter(wrapTo180(long_all(ii)), lat_all(ii), s*ones(274,1), PERI365.North*1000 ,'filled' )
+title('North')
+xlim([-4.5 17])
+subplot(1,4,[3 4]); hold on
+Earth_coast(2)
+xlim([-4.5 17])
+ylim([41 53])
+scatter(wrapTo180(long_all(ii)), lat_all(ii), s*ones(274,1), PERI365.Up*1000 ,'filled' )
 title('Up')
 xlim([-4.5 17])
 colorbar
